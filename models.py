aa49b4c85afc7bb353d376bbf06abaff5490f8e6
Thanh Tran <trantienthanh081298@gmail.com>
2021-01-05 10:45:41 +0700

    add model and training code

---
+++ models.py
@@ -0,0 +1,74 @@
+import torch.nn as nn
+
+class ResNeSt(nn.Module):
+    def __init__(self, pre_model, cfg):
+        super(ResNeSt, self).__init__()
+        self.cfg = cfg
+        self.conv1 = pre_model.conv1
+        self.bn1 = pre_model.bn1
+        self.relu = pre_model.relu
+        self.maxpool = pre_model.maxpool
+        self.layer1 = pre_model.layer1
+        self.layer2 = pre_model.layer2
+        self.layer3 = pre_model.layer3
+        self.layer4 = pre_model.layer4
+        self.avgpool = pre_model.avgpool
+        self.num_features = pre_model.fc.in_features
+        self._init_classifier()
+
+    def _init_classifier(self):
+        for index, num_class in enumerate(self.cfg.num_classes):
+            setattr(self,"fc_"+str(index),
+                    # nn.Conv2d(self.num_features,num_class,kernel_size=1,
+                    #           stride=1,padding=0,bias=True)
+                    nn.Linear(in_features=self.num_features,
+                              out_features=num_class, bias=True)
+                    )
+
+            classifier = getattr(self, "fc_" + str(index))
+            if isinstance(classifier, nn.Conv2d):
+                classifier.weight.data.normal_(0, 0.01)
+                classifier.bias.data.zero_()
+    
+    def forward(self, x):
+        # (N, C, H, W)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        feat_map = self.layer4(x)
+        # [(N, 1), (N,1),...]
+        logits = list()
+        # [(N, H, W), (N, H, W),...]
+        logit_maps = list()
+        for index, num_class in enumerate(self.cfg.num_classes):
+            if self.cfg.attention_map != "None":
+                feat_map = self.attention_map(feat_map)
+
+            classifier = getattr(self, "fc_" + str(index))
+            # (N, 1, H, W)
+            # logit_map = None
+            # if not (self.cfg.global_pool == 'AVG_MAX' or
+            #         self.cfg.global_pool == 'AVG_MAX_LSE'):
+            #     logit_map = classifier(feat_map)
+            #     logit_maps.append(logit_map.squeeze())
+            # (N, C, 1, 1)
+            # feat = self.global_pool(feat_map, logit_map)
+            feat = self.avgpool(feat_map)
+
+            # if self.cfg.fc_bn:
+            #     bn = getattr(self, "bn_" + str(index))
+            #     feat = bn(feat)
+            feat = F.dropout(feat, p=self.cfg.fc_drop, training=self.training)
+            # (N, num_class, 1, 1)
+
+            logit = classifier(feat)
+
+            # (N, num_class)
+            logit = logit
+            logits.append(logit)
+
+        return logits
---
+++ train_wo_logging.py
@@ -0,0 +1,290 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import numpy as np
+from sklearn import metrics
+import time, os
+from tensorboardX import SummaryWriter
+
+def lr_schedule(lr, lr_factor, epoch_now, lr_epochs):
+    """
+    Learning rate schedule with respect to epoch
+    lr: float, initial learning rate
+    lr_factor: float, decreasing factor every epoch_lr
+    epoch_now: int, the current epoch
+    lr_epochs: list of int, decreasing every epoch in lr_epochs
+    return: lr, float, scheduled learning rate.
+    """
+    count = 0
+    for epoch in lr_epochs:
+        if epoch_now >= epoch:
+            count += 1
+            continue
+
+        break
+
+    return lr * np.power(lr_factor, count)
+
+def get_loss(output, target, index, device, cfg):
+    if cfg.criterion == 'BCE':
+        for num_class in cfg.num_classes:
+            assert num_class == 1
+        target = target[:, index].view(-1)
+        pos_weight = torch.from_numpy(
+            np.array(cfg.pos_weight,
+                     dtype=np.float32)).to(device).type_as(target)
+        if cfg.batch_weight:
+            if target.sum() == 0:
+                loss = torch.tensor(0., requires_grad=True).to(device)
+            else:
+                weight = (target.size()[0] - target.sum()) / target.sum()
+                loss = F.binary_cross_entropy_with_logits(
+                    output[index].view(-1), target, pos_weight=weight)
+        else:
+            loss = F.binary_cross_entropy_with_logits(
+                output[index].view(-1), target, pos_weight=pos_weight[index])
+
+        label = torch.sigmoid(output[index].view(-1)).ge(0.5).float()
+        acc = (target == label).float().sum() / len(label)
+    else:
+        raise Exception('Unknown criterion : {}'.format(cfg.criterion))
+
+    return (loss, acc)
+
+def test_epoch(summary, cfg, device, model, dataloader):
+    torch.set_grad_enabled(False)
+    model.eval()
+    steps = len(dataloader)
+    dataiter = iter(dataloader)
+    num_tasks = len(cfg.num_classes)
+
+    loss_sum = np.zeros(num_tasks)
+    acc_sum = np.zeros(num_tasks)
+
+    predlist = list(x for x in range(len(cfg.num_classes)))
+    true_list = list(x for x in range(len(cfg.num_classes)))
+    for step in range(steps):
+        image, target = next(dataiter)
+        image = image.to(device)
+        target = target.to(device)
+        output = model(image)
+        # different number of tasks
+        for t in range(len(cfg.num_classes)):
+
+            loss_t, acc_t = get_loss(output, target, t, device, cfg)
+            # AUC
+            output_tensor = torch.sigmoid(
+                output[t].view(-1)).cpu().detach().numpy()
+            target_tensor = target[:, t].view(-1).cpu().detach().numpy()
+            if step == 0:
+                predlist[t] = output_tensor
+                true_list[t] = target_tensor
+            else:
+                predlist[t] = np.append(predlist[t], output_tensor)
+                true_list[t] = np.append(true_list[t], target_tensor)
+
+            loss_sum[t] += loss_t.item()
+            acc_sum[t] += acc_t.item()
+    summary['loss'] = loss_sum / steps
+    summary['acc'] = acc_sum / steps
+
+    return summary, predlist, true_list
+
+def train_epoch(summary, summary_dev, cfg, device, model, dataloader,
+                dataloader_dev, optimizer, save_path, best_dict, summary_writer):
+    torch.set_grad_enabled(True)
+    model.train()
+    steps = len(dataloader)
+    dataiter = iter(dataloader)
+    label_header = dataloader.dataset._label_header
+    dev_header = dataloader_dev.dataset._label_header
+    num_tasks = len(cfg.num_classes)
+
+    time_now = time.time()
+    loss_sum = np.zeros(num_tasks)
+    acc_sum = np.zeros(num_tasks)
+    for step in range(steps):
+        image, target = next(dataiter)
+        image = image.to(device)
+        target = target.to(device)
+        output = model(image)
+
+        # different number of tasks
+        loss = 0
+        for t in range(num_tasks):
+            loss_t, acc_t = get_loss(output, target, t, device, cfg)
+            loss += loss_t
+            loss_sum[t] += loss_t.item()
+            acc_sum[t] += acc_t.item()
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        summary['step'] += 1
+
+        if summary['step'] % cfg.log_every == 0:
+            time_spent = time.time() - time_now
+            time_now = time.time()
+
+            loss_sum /= cfg.log_every
+            acc_sum /= cfg.log_every
+            loss_str = ' '.join(map(lambda x: '{:.5f}'.format(x), loss_sum))
+            acc_str = ' '.join(map(lambda x: '{:.3f}'.format(x), acc_sum))
+
+            print(
+                'Train, Epoch : {}, Step : {}, Loss : {}, '
+                'Acc : {}, Run Time : {:.2f} sec'
+                .format(summary['epoch'] + 1, summary['step'], loss_str,
+                        acc_str, time_spent))
+
+            for t in range(num_tasks):
+                summary_writer.add_scalar(
+                    'train/loss_{}'.format(label_header[t]), loss_sum[t],
+                    summary['step'])
+                summary_writer.add_scalar(
+                    'train/acc_{}'.format(label_header[t]), acc_sum[t],
+                    summary['step'])
+
+            loss_sum = np.zeros(num_tasks)
+            acc_sum = np.zeros(num_tasks)
+
+        if summary['step'] % cfg.test_every == 0:
+            time_now = time.time()
+            summary_dev, predlist, true_list = test_epoch(
+                summary_dev, cfg, device, model, dataloader_dev)
+            time_spent = time.time() - time_now
+
+            auclist = []
+            for i in range(len(cfg.num_classes)):
+                y_pred = predlist[i]
+                y_true = true_list[i]
+                fpr, tpr, thresholds = metrics.roc_curve(
+                    y_true, y_pred, pos_label=1)
+                auc = metrics.auc(fpr, tpr)
+                auclist.append(auc)
+            summary_dev['auc'] = np.array(auclist)
+
+            loss_dev_str = ' '.join(map(lambda x: '{:.5f}'.format(x),
+                                        summary_dev['loss']))
+            acc_dev_str = ' '.join(map(lambda x: '{:.3f}'.format(x),
+                                       summary_dev['acc']))
+            auc_dev_str = ' '.join(map(lambda x: '{:.3f}'.format(x),
+                                       summary_dev['auc']))
+
+            print(
+                'Dev, Step : {}, Loss : {}, Acc : {}, Auc : {},'
+                'Mean auc: {:.3f} ''Run Time : {:.2f} sec' .format(
+                    summary['step'],
+                    loss_dev_str,
+                    acc_dev_str,
+                    auc_dev_str,
+                    summary_dev['auc'].mean(),
+                    time_spent))
+
+            for t in range(len(cfg.num_classes)):
+                summary_writer.add_scalar(
+                    'dev/loss_{}'.format(dev_header[t]),
+                    summary_dev['loss'][t], summary['step'])
+                summary_writer.add_scalar(
+                    'dev/acc_{}'.format(dev_header[t]), summary_dev['acc'][t],
+                    summary['step'])
+                summary_writer.add_scalar(
+                    'dev/auc_{}'.format(dev_header[t]), summary_dev['auc'][t],
+                    summary['step'])
+
+            save_best = False
+            mean_acc = summary_dev['acc'][cfg.save_index].mean()
+            if mean_acc >= best_dict['acc_dev_best']:
+                best_dict['acc_dev_best'] = mean_acc
+                if cfg.best_target == 'acc':
+                    save_best = True
+
+            mean_auc = summary_dev['auc'][cfg.save_index].mean()
+            if mean_auc >= best_dict['auc_dev_best']:
+                best_dict['auc_dev_best'] = mean_auc
+                if cfg.best_target == 'auc':
+                    save_best = True
+
+            mean_loss = summary_dev['loss'][cfg.save_index].mean()
+            if mean_loss <= best_dict['loss_dev_best']:
+                best_dict['loss_dev_best'] = mean_loss
+                if cfg.best_target == 'loss':
+                    save_best = True
+            
+            torch.save(
+                    {'epoch': summary['epoch'],
+                     'step': summary['step'],
+                     'acc_dev_best': mean_acc,
+                     'auc_dev_best': mean_auc,
+                     'loss_dev_best': mean_loss,
+                     'state_dict': model.state_dict()},
+                    os.path.join(save_path, 'latest.ckpt')
+                )
+
+            if save_best:
+                torch.save(
+                    {'epoch': summary['epoch'],
+                     'step': summary['step'],
+                     'acc_dev_best': best_dict['acc_dev_best'],
+                     'auc_dev_best': best_dict['auc_dev_best'],
+                     'loss_dev_best': best_dict['loss_dev_best'],
+                     'state_dict': model.state_dict()},
+                    os.path.join(save_path, 'best{}.ckpt'.format(
+                        best_dict['best_idx']))
+                )
+                best_dict['best_idx'] += 1
+                if best_dict['best_idx'] > cfg.save_top_k:
+                    best_dict['best_idx'] = 1
+                print(
+                    'Best, Step : {}, Loss : {}, Acc : {},Auc :{},'
+                    'Best Auc : {:.3f}' .format(
+                        summary['step'],
+                        loss_dev_str,
+                        acc_dev_str,
+                        auc_dev_str,
+                        best_dict['auc_dev_best']))
+            
+        model.train()
+        torch.set_grad_enabled(True)
+    summary['epoch'] += 1
+
+    return summary, best_dict
+
+def train(model, cfg, optimizer, train_loader, val_loader, save_path='./', resume=False, ckpt_path=None):
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    summary_train = {'epoch': 0, 'step': 0}
+    summary_dev = {'loss': float('inf'), 'acc': 0.0}
+    summary_writer = SummaryWriter(save_path)
+    epoch_start = 0
+    best_dict = {
+        "acc_dev_best": 0.0,
+        "auc_dev_best": 0.0,
+        "loss_dev_best": float('inf'),
+        "fused_dev_best": 0.0,
+        "best_idx": 1}
+
+    if resume:
+        if ckpt_path is None:
+            raise Exception("ckp_path is not defined for resume training!")
+        ckpt = torch.load(ckpt_path, map_location=device)
+        model.load_state_dict(ckpt['state_dict'])
+        summary_train = {'epoch': ckpt['epoch'], 'step': ckpt['step']}
+        best_dict['acc_dev_best'] = ckpt['acc_dev_best']
+        best_dict['loss_dev_best'] = ckpt['loss_dev_best']
+        best_dict['auc_dev_best'] = ckpt['auc_dev_best']
+        epoch_start = ckpt['epoch']
+
+    for epoch in range(epoch_start, cfg.epoch):
+        lr = lr_schedule(cfg.lr, cfg.lr_factor, summary_train['epoch'],
+                         cfg.lr_epochs)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+
+        summary_train, best_dict = train_epoch(
+            summary_train, summary_dev, cfg, device, model,
+            train_loader, val_loader, optimizer,
+            save_path, best_dict, summary_writer)
